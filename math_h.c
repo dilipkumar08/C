b37dbaec055eb291226171bb37a5a27eba5a5e36
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2023-12-04 01:21:30 +0530

    Create math_h.c
    this is a simple program which explains some function in the math.h header package
    
    ouput : 
    ceil :11.000000
    floor :10.000000
    Square root : 3.162278
    2 Power n :100.000000

---
+++ math_h.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n=10; float c=10.5;
+    printf("ceil :%f\n",ceil(c));
+    printf("floor :%f\n",floor(c));
+    printf("Square root: %f\n",sqrt(n));
+    printf("2 Power n:%f\n",pow(n,2));
+return 0;
+}
